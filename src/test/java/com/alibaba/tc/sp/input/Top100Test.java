2d2cd594c1e9d87c7185819638af968c9e24b1f2
shanqiang.sq <shanqiang.sq@alibaba-inc.com>
2021-09-27 11:48:49 +0800

    consistent function definition

--- src/test/java/com/alibaba/tc/sp/input/Top100Test.java
+++ src/test/java/com/alibaba/tc/sp/input/Top100Test.java
@@ -150,7 +150,7 @@ public class Top100Test {
                 Table table = kafkaStreamTable.consume();
                 table = table.select(new ScalarFunction() {
                     @Override
-                    public Comparable[] returnOneRow(Row row) throws InterruptedException {
+                    public Comparable[] returnOneRow(Row row) {
                         Row commodity = mysqlDimensionTable.curTable().getRow(row.getInteger("commodity_id"));
                         return new Comparable[]{
                                 commodity.getString("name"),
